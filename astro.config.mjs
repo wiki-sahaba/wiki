0a45115de5792fd3920468ef94c7f68ec75a61bb
wiki-sahaba <wiki-sahaba@users.noreply.github.com>
2026-07-01 05:57:21 +0300

    add GitHub Pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://wiki-sahaba.github.io",
+  base: "/wiki",
   vite: {
     css: {
       preprocessorOptions: {
